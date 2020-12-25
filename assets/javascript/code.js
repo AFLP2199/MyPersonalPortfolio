f275b90b2ff61ac6056bb9e38c07704347238fd1
Agustin Lopez <59517612+AFLP2199@users.noreply.github.com>
2020-12-24 21:38:58 -0300

    Add files via upload

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,1 +1,16 @@
+var hojadeestilo = document.getElementById('stylesheet');
+var button = document.getElementById('change-mode');
+var text = document.getElementById('label');
+var logo = document.getElementById('imagelogo');
 
+button.addEventListener('change' , function(){
+    if(button.checked === true){
+        hojadeestilo.href = "assets/css/dark-mode.css";
+        logo.src = 'assets/images/logo_white.png';
+        text.innerText = "Dark Mode";
+    }else{
+        hojadeestilo.href = "assets/css/light-mode.css";
+        logo.src = 'assets/images/logo_black.png';
+        text.innerText = "Light Mode";
+    }
+});
